1c363b42fb0af42c7395080252983514d0d27cb6
tope alabi <tope.alabi@themechanism.com>
2014-01-14 14:26:50 -0500

    role added to admin user

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,18 +1,19 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :password, :password_confirmation, :role
 
-  ROLES = %w(editor moderator manager)
+  #ROLES = %w(editor moderator manager)
 
-  def role?(base_role)
-    return false unless role
-    ROLES.index(base_role.to_s) <= ROLES.index(role)
-  end
+  #def role?(base_role)
+  #  return false unless role
+  #  ROLES.index(base_role.to_s) <= ROLES.index(role)
+  #end
 
   index do
     column :email
     column :current_sign_in_at
     column :last_sign_in_at
     column :sign_in_count
+    column :role
     default_actions
   end
 
@@ -23,8 +24,24 @@ ActiveAdmin.register AdminUser do
       f.input :email
       f.input :password
       f.input :password_confirmation
+      #f.collection_select :role, AdminUser::ROLES, :to_s, :humanize
+      f.input :role, :as => :select, :collection => [ "Super Admin", "SafeFare Admin", "Content Editor" ]
     end
     f.actions
   end
+
+  controller do
+    def update_resource object, attributes
+      attributes.each do |attr|
+        if attr[:password].blank? and attr[:password_confirmation].blank?
+          attr.delete :password
+          attr.delete :password_confirmation
+        end
+      end
+
+      object.send :update_attributes, *attributes
+    end
+  end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140110154918) do
+ActiveRecord::Schema.define(version: 20140114182753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20140110154918) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
